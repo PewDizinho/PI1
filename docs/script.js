74f8eb33e892392b48626265455bf355281e6b78
PewDizinho <konopkspaulo046@gmail.com>
2023-03-29 01:32:47 -0300

    scriptscript

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,10 @@
-function emailChanges(){
+function emailChanges() {
     let txt = document.getElementById('email')
-    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(txt.value))
-    { 
-        txt.value +='@escola.uniopet.br';
-    
+    if (txt.value.lenght > 0) {
+        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(txt.value)) {
+            txt.value += '@escola.uniopet.br';
+
+        }
+
     }
-    
 }
