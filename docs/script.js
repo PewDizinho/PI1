cb2da31f80e1e55e0b6da94dbee16f24916e9710
PewDizinho <konopkspaulo046@gmail.com>
2023-03-29 01:43:28 -0300

    a

--- docs/script.js
+++ docs/script.js
@@ -3,8 +3,6 @@ function emailChanges() {
     if (txt.value.lenght > 0) {
         if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(txt.value)) {
             txt.value += '@escola.uniopet.br';
-
         }
-
     }
 }
