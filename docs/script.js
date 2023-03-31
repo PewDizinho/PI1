527f7a9d22a13ae5bba7a2b4012ed0512d8dfe14
PewDizinho <konopkspaulo046@gmail.com>
2023-03-31 01:15:44 -0300

    commit

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,39 @@
+const allIds = ["home", "news", "contact", "about"];
+
+
+
 document.getElementById("mybtn").onclick = () => {
     location.href = "./login/index.html";
+
 };
 
-const allIds = ["home", "news", "contact", "about"];
+
 function changeNavBar(wichOne) {
     for (var i of allIds) {
-        document.getElementById(i).className = "desactived";
+        document.getElementById(i).className = "nill";
     }
     document.getElementById(wichOne).className = "active";
+    if (wichOne == "home") {
+        document.getElementById("title").className = "active";
+    } else {
+        document.getElementById("title").className = "title";
+    }
+    switch (wichOne) {
+        case "home":
+
+            break;
+
+        case "new":
+
+            break;
+        case "contact":
+
+            break;
+
+        case "about":
+            location.href = "./subpages/sobre/index.html"
+            break;
+    }
 }
+
+
