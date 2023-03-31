6c2879b3c2c06a542c61f328b45e411fe4598fb8
PewDizinho <konopkspaulo046@gmail.com>
2023-03-31 19:45:03 -0300

    card test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const allIds = ["home", "news", "contact", "about"];
 
+const allIds = ["home", "news", "contact", "about"];
 
 
 function doClickAction() {
@@ -35,3 +35,28 @@ function changeNavBar(wichOne) {
 }
 
 
+function test(name, pfp, role, info) {
+    // Create card element
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const img = document.createElement("img");
+    img.src = pfp;
+    card.appendChild(img);
+
+    const h2 = document.createElement("h2");
+    h2.innerText = name;
+    card.appendChild(h2);
+
+    const span = document.createElement("span");
+    span.innerText = role;
+    card.appendChild(span);
+
+    const p = document.createElement("p");
+    p.innerText = info;
+    card.appendChild(p);
+
+    // Add card to container
+    const container = document.getElementById("card-container");
+    container.appendChild(card);
+}
