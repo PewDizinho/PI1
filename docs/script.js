76cbb2d631143ad7d7d4f37c8de173c2bbfd4a0f
PewDizinho <konopkspaulo046@gmail.com>
2023-03-31 01:44:27 -0300

    error page test

--- docs/script.js
+++ docs/script.js
@@ -12,22 +12,6 @@ function changeNavBar(wichOne) {
     for (var i of allIds) {
         document.getElementById(i).className = "nill";
     }
-    switch (wichOne) {
-        case "home":
-            location.href = "https://PewDizinho.github.io/PI1/"
-            break;
-
-        case "new":
-
-            break;
-        case "contact":
-
-            break;
-
-        case "about":
-            location.href = "https://PewDizinho.github.io/PI1/subpages/sobre/index.html";
-            break;
-    }
     document.getElementById(wichOne).className = "active";
 }
 
