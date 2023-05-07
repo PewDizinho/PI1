21cf65b9b26b9406a8ad42710bfd570a2664d381
PewDizinho <konopkspaulo046@gmail.com>
2023-05-07 17:46:29 -0300

    generateCard button updated to the new buttons

--- generateMonthSellers.js
+++ generateMonthSellers.js
@@ -2,61 +2,73 @@ const monthSellers = [
     {
         imgUrl: "https://pewdizinho.github.io/PI1/assets/fotosZoadas/fundadores/Paulo.jpg",
         name: "Paulo Eduardo Konopka",
-        area: "Programação",
+        curso: "Análise e Desenvolvimento de Sistemas",
+        title: "Programador do mês",
         university: "UniOpet",
-        stars: "NaN",
+        stars: "★★★☆☆☆",
         sellsThisMonth: "69",
         sellAllTime: "169",
+        aboutme: "Sou um entusiasta em programação, tendo conhecimento em desenvolvimento web e mobile, tenho interesse em desenvolvimentos de jogos e design",
         profileUrl: "",
     },
     {
         imgUrl: "https://pewdizinho.github.io/PI1/assets/fotosZoadas/fundadores/josé.jpg",
         name: "José Augusto",
-        area: "Programação",
+        curso: "Análise e Desenvolvimento de Sistemas",
+        title: "Programador do mês",
         university: "UniOpet",
-        stars: "NaN",
+        stars: "★★★☆☆☆",
         sellsThisMonth: "69",
         sellAllTime: "169",
+        aboutme: "Sou um entusiasta em programação, tendo conhecimento em desenvolvimento web e mobile, tenho interesse em desenvolvimentos de jogos e design",
         profileUrl: "",
     },
     {
         imgUrl: "https://pewdizinho.github.io/PI1/assets/fotosZoadas/fundadores/bryan.jpg",
         name: "Bryan Alexandre",
-        area: "Programação",
+        curso: "Análise e Desenvolvimento de Sistemas",
+        title: "Programador do mês",
         university: "UniOpet",
-        stars: "NaN",
+        stars: "★★★☆☆☆",
         sellsThisMonth: "69",
         sellAllTime: "169",
+        aboutme: "Sou um entusiasta em programação, tendo conhecimento em desenvolvimento web e mobile, tenho interesse em desenvolvimentos de jogos e design",
         profileUrl: "",
     },
     {
         imgUrl: "https://pewdizinho.github.io/PI1/assets/fotosZoadas/fundadores/bruno.jpg",
         name: "Bruno Eufrasio",
-        area: "Programação",
+        curso: "Análise e Desenvolvimento de Sistemas",
+        title: "Programador do mês",
         university: "UniOpet",
-        stars: "NaN",
+        stars: "★★★☆☆☆",
         sellsThisMonth: "69",
         sellAllTime: "169",
+        aboutme: "Sou um entusiasta em programação, tendo conhecimento em desenvolvimento web e mobile, tenho interesse em desenvolvimentos de jogos e design",
         profileUrl: "",
     },
     {
         imgUrl: "https://pewdizinho.github.io/PI1/assets/fotosZoadas/fundadores/Trobojão.jpg",
-        name: "João Scarante",
-        area: "Programação",
+        title: "João Scarante",
+        curso: "Análise e Desenvolvimento de Sistemas",
+        titulo: "Programador do mês",
         university: "UniOpet",
-        stars: "NaN",
+        stars: "★★★☆☆☆",
         sellsThisMonth: "69",
         sellAllTime: "169",
+        aboutme: "Sou um entusiasta em programação, tendo conhecimento em desenvolvimento web e mobile, tenho interesse em desenvolvimentos de jogos e design",
         profileUrl: "",
     },
     {
         imgUrl: "https://pewdizinho.github.io/PI1/assets/fotosZoadas/fundadores/JoãoTrizotte.jpg",
         name: "João Trizotte",
-        area: "Programação",
+        curso: "Análise e Desenvolvimento de Sistemas",
+        title: "Programador do mês",
         university: "UniOpet",
-        stars: "NaN",
+        stars: "★★★☆☆☆",
         sellsThisMonth: "69",
         sellAllTime: "169",
+        aboutme: "Sou um entusiasta em programação, tendo conhecimento em desenvolvimento web e mobile, tenho interesse em desenvolvimentos de jogos e design",
         profileUrl: "",
     },
 ]
@@ -70,7 +82,7 @@ function generateCard() {
     for (let seller of monthSellers) {
         document.getElementById("gerarCardButton").style.display = "none";
         const container = document.getElementById("menuBoxInside");
-        container.appendChild(new SellerOfTheMonthCard(seller).getCard);
+        container.appendChild(new SellerOfTheMonthCard(seller).newCard);
     }
 
 }
@@ -79,6 +91,83 @@ class SellerOfTheMonthCard {
 
     constructor(obj) {
         this.seller = obj;
+    }
+    get newCard() {
+        const card = document.createElement("div");
+        const flipCardInner = document.createElement("div");
+        const flipCardFront = document.createElement("div");
+        const flipCardback = document.createElement("div");
+        const infoLeft = document.createElement("div");
+        const infoRight = document.createElement("div");
+        const spacer = document.createElement("div");
+        const aboutMe = document.createElement("p");
+        const name = document.createElement("p");
+        const title = document.createElement("p");
+        const area = document.createElement("p");
+        const university = document.createElement("p");
+        const stars = document.createElement("p");
+        const sellsThisMonth = document.createElement("p");
+        const sellAllTime = document.createElement("p");
+        const img = document.createElement("img");
+        const imgBack = document.createElement("img");
+
+
+
+        aboutMe.innerText = `"${this.seller.aboutme}"`;
+        name.innerText = this.seller.name;
+        title.innerText = this.seller.title;
+        area.innerText = this.seller.curso;
+        stars.innerText = this.seller.stars;
+        sellsThisMonth.innerText = `Vendas realizadas esse mês: ${this.seller.sellsThisMonth}`;
+        sellAllTime.innerText = `Vendas realizadas no total: ${this.seller.sellAllTime}`;
+        img.src = this.seller.imgUrl;
+        imgBack.src = this.seller.imgUrl;
+
+        card.className = "flip-card";
+        flipCardInner.className = "flip-card-inner";
+        flipCardFront.className = "flip-card-front";
+        flipCardback.className = "flip-card-back";
+        title.className = "menuBoxInfo txtShadow";
+        name.className = "menuBoxInfo txtShadow";
+        area.className = "menuBoxInfo txtShadow";
+        aboutMe.className = "menuBoxInfo txtShadow";
+
+        university.className = "menuBoxInfo txtShadow";
+        stars.className = "menuBoxInfo txtShadow";
+        sellsThisMonth.className = "menuBoxInfo txtShadow";
+        sellAllTime.className = "menuBoxInfo txtShadow";
+        spacer.className = "spacer";
+        img.className = "menuBoxIcon";
+        imgBack.className = "menuBoxIcon";
+        infoLeft.className = "menuBoxInfo";
+        infoRight.className = "menuBoxInfo";
+
+
+
+        infoLeft.appendChild(name);
+        infoLeft.appendChild(title);
+        infoLeft.appendChild(area);
+        flipCardFront.appendChild(img);
+
+        infoRight.appendChild(sellsThisMonth);
+        infoRight.appendChild(sellAllTime);
+        infoRight.appendChild(stars);
+        flipCardFront.appendChild(infoLeft);
+        flipCardFront.appendChild(spacer);
+        flipCardFront.appendChild(infoRight);
+
+
+
+
+        flipCardback.appendChild(imgBack);
+        flipCardback.appendChild(aboutMe)
+
+        flipCardInner.appendChild(flipCardFront);
+        flipCardInner.appendChild(flipCardback);
+        card.appendChild(flipCardInner);
+
+
+        return card;
     }
     get getCard() {
         const card = document.createElement("div");
@@ -103,6 +192,7 @@ class SellerOfTheMonthCard {
         stars.innerText = `Nota: ${this.seller.stars}`;
         sellsThisMonth.innerText = `Vendas realizadas esse mês: ${this.seller.sellsThisMonth}`;
         sellAllTime.innerText = `Vendas realizadas no total: ${this.seller.sellAllTime}`;
+
         name.className = "menuBoxInfo";
         area.className = "menuBoxInfo";
         university.className = "menuBoxInfo";
@@ -123,4 +213,6 @@ class SellerOfTheMonthCard {
 
         return card;
     }
+
+
 }
